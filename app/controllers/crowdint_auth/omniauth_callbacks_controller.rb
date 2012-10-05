1184766e946292daccb91a9ba4c45e3be1b91f52
David Padilla <david@crowdint.com>
2012-10-05 18:14:32 -0500

    Finished the omniauth callbacks controller

--- app/controllers/crowdint_auth/omniauth_callbacks_controller.rb
+++ app/controllers/crowdint_auth/omniauth_callbacks_controller.rb
@@ -6,4 +6,20 @@ class CrowdintAuth::OmniauthCallbacksController < Devise::OmniauthCallbacksContr
   def create_user_record(email, name)
     user_class.create(:email => email, :name => name)
   end
+
+  def google_apps
+    auth_hash = request.env['omniauth.auth']
+    email = auth_hash.info['email']
+
+    user = user_class.find_by_email(email)
+    user ||= create_user_record(email, auth_hash.info['name'])
+
+    if user.persisted?
+      sign_in_and_redirect user
+    end
+  end
+
+  def google_apps_sign_in
+    redirect_to user_omniauth_authorize_path :google_apps
+  end
 end
