c523cabd53c57990bf5bd704971edbf3dddbd8e2
David Padilla <david@crowdint.com>
2012-10-07 16:54:01 -0500

    Custom domains

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,6 @@
 require 'devise'
 Devise.setup do |config|
-  config.omniauth :google_apps, :domain => 'crowdint.com',
+  config.omniauth :google_apps, :domain => "#{(ENV['GOOGLE_APPS_AUTH_DOMAIN'] || 'crowdint.com')}",
       :strategy => OmniAuth::Strategies::GoogleApps
 
   config.warden { |manager| manager.failure_app = CrowdintAuth::Devise::FailureApp }
